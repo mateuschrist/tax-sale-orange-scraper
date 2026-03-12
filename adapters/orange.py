c7e4c8da08c1f866d5222127f337aa7eca948304
mateuschrist <mateus.christ.eua@gmail.com>
2026-03-12 12:33:51 -0400

    Update orange.py

--- adapters/orange.py
+++ adapters/orange.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 
 def run():
     # Importa e executa o runner do scraper principal
-    from scraper import run as run_scraper
-    run_scraper()
+    from scraper import run
+    run()
